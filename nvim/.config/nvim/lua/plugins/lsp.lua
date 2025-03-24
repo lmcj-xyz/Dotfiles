6e2f112374cfeee3c3edb0d27b71d6fa6e5c0f19
LMCJ T470 <dev@lmcj.xyz>
2025-03-24 13:31:56 +0000

    ltex dictionaries configured

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -20,12 +20,19 @@ return {
                 },
             })
             lspconfig.lua_ls.setup({}) --installed from releases
+            local eng_dict = {}
+            for word in io.open(vim.fn.stdpath("config") .. "/spell/en.utf-8.add", "r"):lines() do
+                table.insert(eng_dict, word)
+            end
             lspconfig.ltex.setup({ --installed from releases
                 filetypes = { "latex", "tex", "bib", },
                 settings = {
                     ltex = {
                         enabled = {'bib', 'context', 'latex', 'plaintex', 'tex'},
                         language = 'en-GB',
+                        dictionary = {
+                            ["en-GB"] = eng_dict,
+                        },
                     }
                 }
             })
